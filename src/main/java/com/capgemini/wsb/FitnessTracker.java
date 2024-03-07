358541c5836581e98183fec1cc53bf53ae6a6d25
szymond <szymond.rekinysukcesu@gmail.com>
2024-03-07 19:57:40 +0100

    change dependency

--- src/main/java/com/capgemini/wsb/FitnessTracker.java
+++ src/main/java/com/capgemini/wsb/FitnessTracker.java
@@ -3,6 +3,7 @@ package com.capgemini.wsb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class FitnessTracker {
 
     public static void main(String[] args) {
